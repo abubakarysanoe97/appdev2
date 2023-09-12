9764398b14c883e3e16c2437afdd83922bd50257
abubakarysanoe97 <abubakarysanoe97@gmail.com>
2023-09-12 10:49:02 -0400

    done homework

---
+++ src/main/java/org/example/GameLauncher.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessingGame game = new GuessingGame();
+        game.startGame();
+    }
+}
